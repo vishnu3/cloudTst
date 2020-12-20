808c994601788f1a06ec2f2ef13cb4147329d7f9
vishnu3 <jethliyavishnu@gmail.com>
2020-12-20 21:32:46 +0530

    enable mvn

---
+++ src/main/java/com/example/demo/CloudApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApplication.class, args);
+
+		System.out.println("hi Boss !! Going Local to Cloud");
+	}
+
+}
